1090a7f8d10d47b30bd676b12e167dd137fe1e65
LuchoBre <lucho.bregoli@gmail.com>
2023-11-22 18:53:18 -0300

    Se agrego el json de vercel

--- src/services/oneMovieApi.js
+++ src/services/oneMovieApi.js
@@ -2,8 +2,8 @@ const API_KEY = 'ac0627bfbad8d649f13be58b7907a6b1'
 const LANGUAGE = 'es-ES';
 
 export const fetchMovie = async (movieId) => {
+  const URL_ONEMOVIE = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=${LANGUAGE}`;
   try {
-    const URL_ONEMOVIE = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=${LANGUAGE}`;
     const response = await fetch(URL_ONEMOVIE);
     if (response.ok) {
       return await response.json();
